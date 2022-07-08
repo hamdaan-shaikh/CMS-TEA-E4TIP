6d366d13f9a319880224b4da17a5421f89b1613c
hash843d <hamdaan_asif.shaikh@mailbox.tu-dresden.de>
2022-07-08 10:13:50 +0200

    Added Popup Modal component as template, implemented it as User Info in Overview Screen

---
+++ CMS_TEA_ET4IP_Mob_App/src/commons/components/PopupModal.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+
+// Components
+import {widthPercentageToDP as WP} from 'react-native-responsive-screen';
+import Modal from 'react-native-modal';
+
+// Styles
+import {MAIN_STYLES} from '../styles/main-styles';
+
+const PopupModal = props => {
+  return (
+    <Modal
+      isVisible={props.visible}
+      coverScreen={props.coverScreen ?? true}
+      useNativeDriverForBackdrop={true}
+      useNativeDriver={true}
+      animationInTiming={200}
+      animationOutTiming={200}
+      animationIn="fadeIn"
+      animationOut="fadeOut"
+      style={LOCAL_STYLES.MAIN_CONTAINER}
+      onBackdropPress={props.closeModal}
+      onBackButtonPress={props.closeModal}>
+      <View style={[LOCAL_STYLES.COMPONENT_CONTAINER, props.style]}>
+        {props.children}
+      </View>
+    </Modal>
+  );
+};
+
+const UserInfoRow = props => {
+  return (
+    <View style={LOCAL_STYLES.ROW_CONTAINER}>
+      <Text style={LOCAL_STYLES.ROW_TITLE}>{props.title + ': '}</Text>
+      <Text style={LOCAL_STYLES.ROW_SUBTITLE}>{props.subtitle}</Text>
+    </View>
+  );
+};
+
+export default props => {
+  return (
+    <PopupModal visible={props.visible} closeModal={props.closeModal}>
+      <View style={LOCAL_STYLES.USER_INFO_CONTAINER}>
+        <View style={LOCAL_STYLES.LEFT_CONTAINER}>
+          <View
+            style={{
+              aspectRatio: 1,
+              height: 90,
+              backgroundColor: '#c0c',
+              borderRadius: 4,
+            }}
+          />
+        </View>
+        <View style={LOCAL_STYLES.RIGHT_CONTAINER}>
+          <UserInfoRow title="Name" subtitle="Hamdaan Shaikh" />
+          <UserInfoRow title="Age" subtitle="25" />
+          <UserInfoRow title="Gender" subtitle="Male" />
+          <UserInfoRow title="Address" subtitle="Borsbergstrasse 34" />
+          <UserInfoRow title="Post Code" subtitle="01309" />
+          <UserInfoRow title="City" subtitle="Dresden" />
+          <UserInfoRow title="Country" subtitle="Germany" />
+          <UserInfoRow title="Phone" subtitle="+49-123-456-7890" />
+          <UserInfoRow title="Email" subtitle="hash843d@tud.de" />
+          <UserInfoRow title="Relationship Status" subtitle="-" />
+          <UserInfoRow title="Study at" subtitle="TU Dresden" />
+          <UserInfoRow title="Work at" subtitle="-" />
+        </View>
+      </View>
+    </PopupModal>
+  );
+};
+
+const LOCAL_STYLES = StyleSheet.create({
+  MAIN_CONTAINER: {
+    ...MAIN_STYLES.CENTER_CONTENT,
+  },
+  COMPONENT_CONTAINER: {
+    borderRadius: WP('3%'),
+    overflow: 'hidden',
+    width: WP('95%'),
+    backgroundColor: '#fff',
+    ...MAIN_STYLES.CENTER_CONTENT,
+  },
+
+  ROW_CONTAINER: {
+    flexDirection: 'row',
+  },
+  ROW_TITLE: {
+    color: '#333',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  ROW_SUBTITLE: {
+    color: '#333',
+    fontSize: 20,
+  },
+
+  USER_INFO_CONTAINER: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+  },
+  LEFT_CONTAINER: {
+    flex: 1,
+    ...MAIN_STYLES.CENTER_CONTENT,
+  },
+  RIGHT_CONTAINER: {
+    flex: 0,
+  },
+});
--- CMS_TEA_ET4IP_Mob_App/src/screens/OverviewScreen.js
+++ CMS_TEA_ET4IP_Mob_App/src/screens/OverviewScreen.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 // Components
 import DataOverviewGraph from '../commons/components/DataOverviewGraph';
+import PopupModal from '../commons/components/PopupModal';
 import PrivacyBadge from '../commons/components/PrivacyBadge';
 
 // Styles
 import {MAIN_STYLES} from '../commons/styles/main-styles';
 
 export default ({navigation}) => {
+  const [state, setState] = useState({
+    modalVisible: false,
+  });
+
+  const toggleModal = modalVisible => {
+    setState(prev => ({
+      ...prev,
+      modalVisible,
+    }));
+  };
+  
   return (
     <View style={LOCAL_STYLES.MAIN_CONTAINER}>
       <DataOverviewGraph />
       <View style={LOCAL_STYLES.BUTTON_CONTAINER}>
-        <Button title="User" onPress={() => {}} />
+      <Button
+          title="User"
+          onPress={() => {
+            toggleModal(true);
+          }}
+        />
         <Button title="Take" onPress={() => {}} />
         <Button
           title="Predictable Behaviour"
@@ -23,6 +40,12 @@ export default ({navigation}) => {
         />
       </View>
       <PrivacyBadge />
+      <PopupModal
+        visible={state.modalVisible}
+        closeModal={() => {
+          toggleModal(false);
+        }}
+      />
     </View>
   );
 };
